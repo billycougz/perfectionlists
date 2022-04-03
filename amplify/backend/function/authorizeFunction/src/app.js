39594b4edb08ba11667b538221ae243fb2035316
billycougz <billycougz@gmail.com>
2022-04-03 19:14:29 -0400

    Refactored secret handling

--- amplify/backend/function/authorizeFunction/src/app.js
+++ amplify/backend/function/authorizeFunction/src/app.js
@@ -19,13 +19,17 @@ app.listen(3000, function () {
   console.log('App started');
 });
 
-// Get secrets from AWS SSM and return in the form { Name: 'secretName', Value: 'secretValue', ... }[]
-const getSecrets = async () => {
-  const Names = ['CLIENT_ID', 'CLIENT_SECRET', 'REDIRECT_URI', 'FRONTEND_URI'].map(
-    (secretName) => process.env[secretName]
-  );
-  const { Parameters } = await new aws.SSM().getParameters({ Names, WithDecryption: true }).promise();
-  return Parameters;
+// Returns a string array of secret values
+const getSecretValues = async (secretNames) => {
+  // Retrieving parameters one by one to ensure order is maintained
+  const parameterPromises = secretNames.map((secretName) => {
+    // An environment specific prefix is applied to the base secret name and stored as an env variable
+    const Name = process.env[secretName];
+    return new aws.SSM().getParameter({ Name, WithDecryption: true }).promise();
+  });
+  const parameterResponses = await Promise.all(parameterPromises);
+  // Each response contains a Parameter object in the form of { Name: 'secretName', Value: 'secretValue', ... }
+  return parameterResponses.map((response) => response.Parameter.Value);
 };
 
 // Cookie provides protection against CSRF attacks
@@ -34,7 +38,8 @@ const AUTH_COOKIE_NAME = 'spotify-toolbox-auth';
 // Spotify Login - Request User Authorization
 app.get('/authorize/login', async (req, res) => {
   console.log('/login');
-  const { CLIENT_ID, REDIRECT_URI } = await getSecrets();
+  const [client_id, redirect_uri] = await getSecretValues(['CLIENT_ID', 'REDIRECT_URI']);
+  console.log(client_id, redirect_uri);
   const state = randomstring.generate(16);
   const scope =
     'user-read-private user-read-email user-read-recently-played user-top-read user-follow-read user-follow-modify playlist-read-private playlist-read-collaborative playlist-modify-public playlist-modify-private';
@@ -43,10 +48,10 @@ app.get('/authorize/login', async (req, res) => {
     'https://accounts.spotify.com/authorize?' +
       querystring.stringify({
         response_type: 'code',
-        client_id: CLIENT_ID,
-        scope: scope,
-        redirect_uri: REDIRECT_URI,
-        state: state,
+        client_id,
+        scope,
+        redirect_uri,
+        state,
       })
   );
 });
@@ -54,7 +59,12 @@ app.get('/authorize/login', async (req, res) => {
 // Spotify Login Callback - Request Access Token
 app.get('/authorize/callback', async (req, res) => {
   console.log('/callback');
-  const { CLIENT_ID, CLIENT_SECRET, REDIRECT_URI, FRONTEND_URI } = await getSecrets();
+  const [client_id, client_secret, redirect_uri, frontend_uri] = await getSecretValues([
+    'CLIENT_ID',
+    'CLIENT_SECRET',
+    'REDIRECT_URI',
+    'FRONTEND_URI',
+  ]);
   const code = req.query.code || null;
   const state = req.query.state || null;
   const storedState = req.cookies ? req.cookies[AUTH_COOKIE_NAME] : null;
@@ -65,11 +75,11 @@ app.get('/authorize/callback', async (req, res) => {
       url: 'https://accounts.spotify.com/api/token',
       form: {
         code: code,
-        redirect_uri: REDIRECT_URI,
+        redirect_uri,
         grant_type: 'authorization_code',
       },
       headers: {
-        Authorization: 'Basic ' + new Buffer(CLIENT_ID + ':' + CLIENT_SECRET).toString('base64'),
+        Authorization: 'Basic ' + new Buffer(client_id + ':' + client_secret).toString('base64'),
       },
       json: true,
     };
@@ -79,7 +89,7 @@ app.get('/authorize/callback', async (req, res) => {
         const access_token = body.access_token;
         const refresh_token = body.refresh_token;
         res.redirect(
-          `${FRONTEND_URI}/#${querystring.stringify({
+          `${frontend_uri}/#${querystring.stringify({
             access_token,
             refresh_token,
           })}`
@@ -94,11 +104,11 @@ app.get('/authorize/callback', async (req, res) => {
 // Spotify Refresh Token - Request a refreshed Access Token
 app.get('/authorize/refresh_token', async (req, res) => {
   console.log('/refresh_token');
-  const { CLIENT_ID, CLIENT_SECRET } = await getSecrets();
+  const [client_id, client_secret] = await getSecretValues(['CLIENT_ID', 'CLIENT_SECRET']);
   const refresh_token = req.query.refresh_token;
   const authOptions = {
     url: 'https://accounts.spotify.com/api/token',
-    headers: { Authorization: 'Basic ' + new Buffer(CLIENT_ID + ':' + CLIENT_SECRET).toString('base64') },
+    headers: { Authorization: 'Basic ' + new Buffer(client_id + ':' + client_secret).toString('base64') },
     form: {
       grant_type: 'refresh_token',
       refresh_token: refresh_token,
